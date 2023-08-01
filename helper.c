5dd38384dc79308f86a9b8fc69754d6c6de3410b
afr117 <alfredo.figueroa07@yahoo.com>
2023-07-31 17:57:42 -0700

    1..100

--- helper.c
+++ helper.c
@@ -1,13 +1,26 @@
+/* helper.c */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "shell.h"
 
+/**
+ * read_line - Read a line of input from the user in interactive mode.
+ *
+ * Return: The read input line or NULL on failure.
+ */
 char *read_line(void)
 {
-	/* Implement function read a line of input from user (interactive mode) */
+	/* Implement function to read a line of input from the user (interactive mode) */
 	return (NULL); /* Replace NULL with the read input line */
 }
 
+/**
+ * read_line_from_file - Read a line from a file.
+ * @stream: The FILE stream to read from.
+ *
+ * Return: The read input line from the file or NULL on failure.
+ */
 char *read_line_from_file(FILE *stream)
 {
 	/* Implement function to read a line from a file */
@@ -15,6 +28,12 @@ char *read_line_from_file(FILE *stream)
 	return (NULL); /* Replace NULL with the read input line from the file */
 }
 
+/**
+ * _strdup - Duplicate a string.
+ * @str: The string to duplicate.
+ *
+ * Return: The duplicated string or NULL on failure.
+ */
 char *_strdup(char *str)
 {
 	/* Implement function to duplicate a string */
