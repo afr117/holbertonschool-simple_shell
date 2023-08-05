423dfe2a9db276bb661ba7eaee5ea55f2f5da53d
afr117 <alfredo.figueroa07@yahoo.com>
2023-08-04 18:19:47 -0700

    1..100

--- shell.c
+++ shell.c
@@ -1,37 +1,31 @@
-/* shell.c */
-
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "shell.h"
 
-/**
- * main - Entry point of the simple shell program.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	char *line;
-	char **commands;
-	int status = 1; /* Shell status (1: active, 0: exit) */
+int main(void) {
+    char *line;
+    char **commands;
+    int status = 1; /* Shell status (1: active, 0: exit) */
 
-	while (status)
-	{
-		printf("($) ");
-		line = read_line(); /* Read input line */
-		if (!line)
-			break;
+    while (status) {
+        printf("#cisfun$ ");
+        line = read_line(); /* Read input line */
+        
+        if (!line) {
+            break; /* Handle EOF */
+        }
+        
+        commands = split_commands(line); /* Split input into commands */
 
-		commands = split_commands(line); /* Split input into commands */
+        if (commands) {
+            status = execute_commands(commands); /* Execute commands */
+            free(commands);
+        }
+        
+        free(line);
+    }
 
-		if (commands)
-		{
-			status = execute_commands(commands); /* Execute commands */
-			free(commands);
-		}
-		free(line);
-	}
-
-	return (0);
+    return 0;
 }
 
