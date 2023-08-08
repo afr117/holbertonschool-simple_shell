a66a7ac93cbe7122922961ac880b0c49932854d8
afr117 <alfredo.figueroa07@yahoo.com>
2023-08-07 18:50:17 -0700

    1..100

--- shell.c
+++ shell.c
@@ -15,7 +15,7 @@ int main(void) {
     char *line;
     char **commands;
     int status = 1; /* Shell status (1: active, 0: exit) */
-    int i; /* Declare 'i' outside of the loop in C89 */
+    int i;
 
     while (status) {
         printf("($) ");
@@ -25,26 +25,11 @@ int main(void) {
 
         commands = lsh_split_line(line);
         if (commands) {
-            pid_t pid;
-            int exec_status;
-
-            pid = fork();
-            if (pid == 0) {
-                /* Child process */
-                if (execvp(commands[0], commands) == -1) {
-                    perror("shell");
-                }
-                exit(EXIT_FAILURE);
-            } else if (pid < 0) {
-                /* Error forking */
-                perror("shell");
-            } else {
-                /* Parent process */
-                waitpid(pid, &exec_status, 0);
-            }
+            if (lsh_execute(commands) == 0)
+                status = 0;
 
             for (i = 0; commands[i] != NULL; i++) {
-                free(commands[i]); /* Free each command */
+                free(commands[i]);
             }
             free(commands);
         }
@@ -63,7 +48,7 @@ char *lsh_read_line(void) {
 
 char **lsh_split_line(char *line) {
     int bufsize = LSH_TOK_BUFSIZE, position = 0;
-    char **tokens = malloc(bufsize * sizeof(char *));
+    char **tokens = (char **)malloc(bufsize * sizeof(char *));
     char *token;
 
     if (!tokens) {
@@ -71,23 +56,42 @@ char **lsh_split_line(char *line) {
         exit(EXIT_FAILURE);
     }
 
-    token = strtok(line, " "); /* Simplified delimiter */
+    token = strtok(line, " ");
     while (token != NULL) {
         tokens[position] = token;
         position++;
 
         if (position >= bufsize) {
             bufsize += LSH_TOK_BUFSIZE;
-            tokens = realloc(tokens, bufsize * sizeof(char *));
+            tokens = (char **)realloc(tokens, bufsize * sizeof(char *));
             if (!tokens) {
                 fprintf(stderr, "shell: allocation error\n");
                 exit(EXIT_FAILURE);
             }
         }
 
-        token = strtok(NULL, " "); /* Simplified delimiter */
+        token = strtok(NULL, " ");
     }
     tokens[position] = NULL;
     return tokens;
+}
+
+int lsh_execute(char **commands) {
+    pid_t pid;
+    int status = 1;
+
+    pid = fork();
+    if (pid == 0) {
+        if (execvp(commands[0], commands) == -1) {
+            perror("shell");
+            exit(EXIT_FAILURE);
+        }
+    } else if (pid < 0) {
+        perror("shell");
+    } else {
+        waitpid(pid, &status, 0);
+    }
+
+    return status;
 }
 
