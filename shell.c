a39f4a48e9dea1483ac75acdb12005f83a429f49
afr117 <alfredo.figueroa07@yahoo.com>
2023-08-07 18:01:55 -0700

    1..100

--- shell.c
+++ shell.c
@@ -27,46 +27,28 @@ int main(void) {
         if (commands) {
             pid_t pid;
             int exec_status;
-            char *path, *token, *full_path;
 
-            path = getenv("PATH");
-            token = strtok(path, ":");
-            while (token) {
-                full_path = (char *)malloc(strlen(token) + strlen(commands[0]) + 2);
-                if (full_path) {
-                    strcpy(full_path, token);
-                    strcat(full_path, "/");
-                    strcat(full_path, commands[0]);
-
-                    if (access(full_path, X_OK) == 0) {
-                        pid = fork();
-                        if (pid == 0) {
-                            /* Child process */
-                            if (execvp(full_path, commands) == -1) {
-                                perror("shell");
-                            }
-                            exit(EXIT_FAILURE);
-                        } else if (pid < 0) {
-                            /* Error forking */
-                            perror("shell");
-                        } else {
-                            /* Parent process */
-                            waitpid(pid, &exec_status, 0);
-                        }
-                        break;
-                    }
-
-                    free(full_path);
+            pid = fork();
+            if (pid == 0) {
+                /* Child process */
+                if (execvp(commands[0], commands) == -1) {
+                    perror("shell");
                 }
-                token = strtok(NULL, ":");
+                exit(EXIT_FAILURE);
+            } else if (pid < 0) {
+                /* Error forking */
+                perror("shell");
+            } else {
+                /* Parent process */
+                waitpid(pid, &exec_status, 0);
             }
 
             for (i = 0; commands[i] != NULL; i++) {
                 free(commands[i]); /* Free each command */
             }
             free(commands);
-            free(line); /* Free the input line after processing */
         }
+        free(line);
     }
 
     return 0;
@@ -81,7 +63,7 @@ char *lsh_read_line(void) {
 
 char **lsh_split_line(char *line) {
     int bufsize = LSH_TOK_BUFSIZE, position = 0;
-    char **tokens = (char **)malloc(bufsize * sizeof(char *));
+    char **tokens = malloc(bufsize * sizeof(char *));
     char *token;
 
     if (!tokens) {
@@ -89,21 +71,21 @@ char **lsh_split_line(char *line) {
         exit(EXIT_FAILURE);
     }
 
-    token = strtok(line, " \t\r\n\a"); /* Simplified delimiter */
+    token = strtok(line, " "); /* Simplified delimiter */
     while (token != NULL) {
         tokens[position] = token;
         position++;
 
         if (position >= bufsize) {
             bufsize += LSH_TOK_BUFSIZE;
-            tokens = (char **)realloc(tokens, bufsize * sizeof(char *));
+            tokens = realloc(tokens, bufsize * sizeof(char *));
             if (!tokens) {
                 fprintf(stderr, "shell: allocation error\n");
                 exit(EXIT_FAILURE);
             }
         }
 
-        token = strtok(NULL, " \t\r\n\a"); /* Simplified delimiter */
+        token = strtok(NULL, " "); /* Simplified delimiter */
     }
     tokens[position] = NULL;
     return tokens;
