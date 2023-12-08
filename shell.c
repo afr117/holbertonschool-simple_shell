3d8f31684f2262ddf17ea3876340344ab1f5aca6
afr117 <alfredo.figueroa07@yahoo.com>
2023-12-08 01:33:17 +0000

    test9

--- shell.c
+++ shell.c
@@ -9,101 +9,105 @@
 
 char *lsh_read_line(void);
 char **lsh_split_line(char *line);
+int lsh_execute(char **commands);
 
+
+/* Main function for the simple shell program */
 int main(void)
 {
-    char *line;
-    char **commands;
-    int status = 1; /* Shell status (1: active, 0: exit) */
-    int i;          /* Declare 'i' outside of the loop in C89 */
+	char *line;
+	char **commands;
+	int status = 1; /* Shell status (1: active, 0: exit) */
+	int i; /* Declare 'i' outside of the loop in C89 */
 
-    while (status)
-    {
-        printf("($) ");
-        line = lsh_read_line();
-        if (!line)
-            break;
+	while (status)
+	{
+		printf("($) ");
+		line = lsh_read_line();
+	if (!line)
+		break;
 
-        commands = lsh_split_line(line);
-        if (commands)
-        {
-            pid_t pid;
-            int exec_status;
+		commands = lsh_split_line(line);
+	if (commands)
+	{
+		pid_t pid;
+			int exec_status;
 
-            pid = fork();
-            if (pid == 0)
-            {
-                /* Child process */
-                if (execvp(commands[0], commands) == -1)
-                {
-                    perror("shell");
-                    exit(EXIT_FAILURE);
-                }
-            }
-            else if (pid < 0)
-            {
-                /* Error forking */
-                perror("shell");
-            }
-            else
-            {
-                /* Parent process */
-                waitpid(pid, &exec_status, 0);
-            }
+		pid = fork();
+	if (pid == 0)
+	{
+		/* Child process */
+	if (execvp(commands[0], commands) == -1)
+{
+		perror("shell");
+}
+		exit(EXIT_FAILURE);
+		}
+		else if (pid < 0)
+{
+		/* Error forking */
+			perror("shell");
+		}
+		else
+{
+		/* Parent process */
+			waitpid(pid, &exec_status, 0);
+		}
 
-            for (i = 0; commands[i] != NULL; i++)
-            {
-                free(commands[i]); /* Free each command */
-            }
-            free(commands);
-        }
-        free(line);
-    }
+	for (i = 0; commands[i] != NULL; i++)
+		{
+		free(commands[i]); /* Free each command */
+		}
+		free(commands);
+		}
+		free(line);
+		}
 
-    return 0;
+	return (0);
 }
 
+/* Read a line of input from the user */
 char *lsh_read_line(void)
 {
-    char *line = NULL;
-    size_t bufsize = 0;
+	char *line = NULL;
+	size_t bufsize = 0;
 
-    getline(&line, &bufsize, stdin);
-    return line;
+	getline(&line, &bufsize, stdin);
+	return (line);
 }
 
+/* Split a line into tokens */
 char **lsh_split_line(char *line)
 {
-    int bufsize = LSH_TOK_BUFSIZE, position = 0;
-    char **tokens = malloc(bufsize * sizeof(char *));
-    char *token;
+	int bufsize = LSH_TOK_BUFSIZE, position = 0;
+	char **tokens = malloc(bufsize * sizeof(char *));
+	char *token;
 
-    if (!tokens)
-    {
-        fprintf(stderr, "shell: allocation error\n");
-        exit(EXIT_FAILURE);
-    }
-
-    token = strtok(line, " "); /* Simplified delimiter */
-    while (token != NULL)
-    {
-        tokens[position] = token;
-        position++;
-
-        if (position >= bufsize)
-        {
-            bufsize += LSH_TOK_BUFSIZE;
-            tokens = realloc(tokens, bufsize * sizeof(char *));
-            if (!tokens)
-            {
-                fprintf(stderr, "shell: allocation error\n");
-                exit(EXIT_FAILURE);
-            }
-        }
-
-        token = strtok(NULL, " "); /* Simplified delimiter */
-    }
-    tokens[position] = NULL;
-    return tokens;
+	if (!tokens)
+	{
+		fprintf(stderr, "shell: allocation error\n");
+		exit(EXIT_FAILURE);
 }
 
+	token = strtok(line, " "); /* Simplified delimiter */
+	while (token != NULL)
+{
+		tokens[position] = token;
+		position++;
+
+	if (position >= bufsize)
+	{
+		bufsize += LSH_TOK_BUFSIZE;
+		tokens = realloc(tokens, bufsize * sizeof(char *));
+	if (!tokens)
+	{
+		fprintf(stderr, "shell: allocation error\n");
+		exit(EXIT_FAILURE);
+}
+}
+
+	token = strtok(NULL, " "); /* Simplified delimiter */
+}
+	tokens[position] = NULL;
+	return (tokens);
+}
